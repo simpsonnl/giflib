3d81eebf25e81a0b2d7d41825c0b088d24543d3e
simpsonnl <simpsonnl22@gmail.com>
2019-10-21 21:21:41 -0400

    basic outline of site with some home page stuff

---
+++ src/main/java/com/nicksimpson/giflib/controller/GifController.java
@@ -0,0 +1,13 @@
+package com.nicksimpson.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String listGifs(){
+        return "home";
+    }
+}
