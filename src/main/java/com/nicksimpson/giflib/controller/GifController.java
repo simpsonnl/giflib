f58ab9f3c2f472f0ff422b72121bd26e9eb411c4
simpsonnl <simpsonnl22@gmail.com>
2019-10-21 22:24:59 -0400

    updated home page with a basic detail page

--- src/main/java/com/nicksimpson/giflib/controller/GifController.java
+++ src/main/java/com/nicksimpson/giflib/controller/GifController.java
@@ -1,13 +1,23 @@
 package com.nicksimpson.giflib.controller;
 
+import com.nicksimpson.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
     @RequestMapping("/")
     public String listGifs(){
         return "home";
+    }
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("cowboy-coder", LocalDate.of(2019,8, 12), "simpsonnl", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
